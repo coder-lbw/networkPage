a1dd2224f7c8142e9ef1c929c6f33c54314483e9
coder-lbw <3113219455@qq.com>
2022-05-02 16:22:29 +0800

    完成了产品中心页面，做了一些适配，修改了一些 bug

--- index.js
+++ index.js
@@ -22,3 +22,20 @@ navEle.onclick = function (e) {
     navEle.classList.toggle('nav-none')
   }
 }
+
+// 产品中心的交互逻辑
+const divEle = document.getElementsByClassName('two-select-cont')
+const labelList = []
+for (let i = 0; i < divEle.length; i++) {
+  const labels = divEle[i].getElementsByTagName('label')
+  labelList.push(...labels)
+}
+let activeLabel = labelList[0]
+
+for (let i = 0; i < divEle.length; i++) {
+  divEle[i].onclick = function (e) {
+    activeLabel.classList.remove('nav-active')
+    e.target.classList.add('nav-active')
+    activeLabel = e.target
+  }
+}
