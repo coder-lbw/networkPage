47d23d60b9cf1716c21e91e0f7bb7b216f9b0153
coder-lbw <3113219455@qq.com>
2022-05-19 21:03:12 +0800

    add about tel and wx gzh

--- index.js
+++ index.js
@@ -39,22 +39,3 @@ for (let i = 0; i < divEle.length; i++) {
     activeLabel = e.target
   }
 }
-
-
-// // 图片懒加载
-// let img = document.getElementsByTagName("img"); 
-// let num = img.length; 
-// let count = 0;//计数器，从第一张图片开始计 
-// lazyload();//首次加载别忘了显示图片 
-// window.addEventListener('scroll', lazyload);
-// function lazyload() { 
-//   let viewHeight = document.documentElement.clientHeight;//视口高度 
-//   let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;//滚动条卷去的高度 
-//   for(let i = count; i <num; i++) { 
-//     // 元素现在已经出现在视口中 
-//     if(img[i].offsetTop < scrollTop + viewHeight) { 
-//       if(img[i].getAttribute("src") !== "default.jpg") continue; img[i].src = img[i].getAttribute("data-src"); 
-//       count ++;
-//     } 
-//   } 
-// }
